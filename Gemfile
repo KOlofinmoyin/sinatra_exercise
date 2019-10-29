3f7f96da6c499a438c2bbcc3328edd3c565a8334
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-10-29 16:23:21 +0000

    added config.ru (not sure why yet?) edited the gem file to include shotgun gem, also installed manually; added session info to app.rb and started app using shotgun"

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
-group :development, :test do
+group :development set :session_secret, :test do
   gem 'rspec'
   gem 'sinatra'
+  gem 'shotgun'
 end
--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'shotgun'
+set :session_secret,
+'super secret'
 
   get '/' do
     "Hello!"
+  end
+
+  get '/secret' do
+    "Hello Kenny, Arjun, Taiwo  Michael!"
   end

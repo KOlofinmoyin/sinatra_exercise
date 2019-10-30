c3ca41fe24edd90bb156062e46ae99da8976ac23
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-10-30 15:14:25 +0000

    concluded the 'Sinatra erb' step>

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'shotgun'
+
 set :session_secret,
 'super secret'
 
@@ -12,14 +12,5 @@ set :session_secret,
   end
 
   get '/cat' do
-    "<HTML>
-    <HEAD>
-    <TITLE>
-    Arjun's first page
-    </TITLE>
-    <div style= 'border: 3px dashed red'>
-    <IMG SRC='http://bit.ly/1eze8aE'>
-    </div>
-    </HEAD>
-    </HTML>"
+    erb(:index)
   end
